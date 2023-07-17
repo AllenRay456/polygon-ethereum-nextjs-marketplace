41e70c88a5828dd3358ad71bfbc413870bc938db
allenRay <yutianriyue@gmail.com>
2023-07-17 17:17:27 +0800

    chore: change config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,20 +8,20 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
+      // url: "https://rpc-mumbai.matic.today",
+      url: "https://polygon-mumbai.infura.io/v3/4c6ab311f61749c087a62d58ca91f1b3",
       accounts: [process.env.privateKey]
     },
     matic: {
       // Infura
       // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
+      // url: "https://rpc-mainnet.maticvigil.com",
+      url: "https://polygon-mainnet.infura.io/v3/4c6ab311f61749c087a62d58ca91f1b3",
       accounts: [process.env.privateKey]
     }
-    */
   },
   solidity: {
     version: "0.8.4",
